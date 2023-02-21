7e8e09c438e55964cf74e3ce2a604c49b4e56eef
BANGLADESH-EMRAN-CYBER <124817167+Emran-cyber99487@users.noreply.github.com>
2023-02-21 08:09:57 +0400

    Create TERMUX-PKG-INSTALL.py

---
+++ TERMUX-PKG-INSTALL.py
@@ -0,0 +1,137 @@
+#Created By Me. 
+#But Giving To Everyone. 
+#Thats Way I Am Uploading 
+#It Open Source😘 
+#Open Source Sc.Don'T Say Its Yours😒
+#FOLLOW ME FOR MORE😘
+
+import os, time, sys
+os.system('clear')
+
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(5. / 100)
+
+logo2='''
+\033[36;1m┌────────────────────────────────────────┐
+\033[36;1m│    \033[37;1m______\033[36;1m__  ___\033[35;1m____  \033[34;1m___    \033[33;1m_   __    │
+\033[36;1m│   \033[37;1m/ ____\033[36;1m/  |/  \033[35;1m/ __ \\\033[34;1m/ / |  \033[33;1m/ | / /    │
+\033[36;1m│  \033[37;1m/ __/ \033[36;1m/ /|_/ \033[35;1m/ /_/ \033[34;1m/ /| | \033[33;1m/  |/ /     │
+\033[36;1m│ \033[37;1m/ /___\033[36;1m/ /  / \033[35;1m/ _, _\033[34;1m/ ___ |\033[33;1m/ /|  /      │
+\033[36;1m│\033[37;1m/_____\033[36;1m/_/  /_\033[35;1m/_/ |_\033[34;1m/_/  |_\033[33;1m/_/ |_/       │
+\033[36;1m└────────────────────────────────────────┘                                                                                                                                                                                                                                                                 
+\033[36;1m┌───────────────────────────────────────┐
+\033[36;1m│ [♣][♠]𝐀𝐔𝐓𝐇𝐎𝐑____𝐄𝐌𝐑𝐀𝐍 𝐇𝐎𝐒𝐒𝐀𝐈𝐍❥        │
+\033[36;1m│ [♣][♠]𝐆𝐈𝐓𝐇𝐔𝐁_____𝐄𝐌𝐑𝐀𝐍-𝐂𝐘𝐁𝐄𝐑𝟗𝟗𝟒𝟖𝟕❥    │
+\033[36;1m│ [♣][♠]𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏___+𝟗𝟕𝟏𝟎𝟓𝟔𝟗𝟓𝟒𝟗𝟖𝟓𝟕❥      │
+\033[36;1m│ [♣][♠]𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊____𝐄𝐌𝐑𝐀𝐍_HOSSAIN       │
+\033[36;1m│ [♣][♠]𝐕𝐄𝐑𝐒𝐈𝐎𝐍____𝐎.𝟐❥                 │
+\033[36;1m│ [♣][♠]𝐅𝐑𝐎𝐌___𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇❥              │
+\033[36;1m│ [♣][♠]𝐖𝐎𝐑𝐊___𝐇𝐀𝐂𝐊𝐈𝐍𝐆_𝐏𝐎𝐆𝐑𝐀𝐌𝐈𝐆❥        │
+\033[36;1m│ [♣][♠]𝐁𝐎𝐗___𝐂𝐘𝐁𝐄𝐑_𝟗𝟗_𝐇𝐀𝐂𝐊𝐈𝐍𝐆_𝐓𝐄𝐀𝐌❥    │
+\033[36;1m│ [♣][♠]𝐏𝐎𝐖𝐄𝐑__𝐁𝐘__\033[1;36m𝐌𝐀𝐗 𝐄𝐌𝐑𝐀𝐍❥\033[1;36m           │
+\033[36;1m└───────────────────────────────────────┘
+'''
+
+print(logo2)
+slowprint('\n Welcome To \033[1;31;40mTermux \033[1;33;40mBasic Installer\n')
+time.sleep(1)
+
+s = ' \033[1;31;40m[\033[1;33;40m1\033[1;31;40m] \033[1;36;40mStart Basic Installation \n \033[1;31;40m[\033[1;33;40m2\033[1;31;40m] \033[1;36;40mShow all package \n \033[1;31;40m[\033[1;33;40m3\033[1;31;40m] \033[1;36;40mFollow on Facebook \n \033[1;31;40m[\033[1;33;40m4\033[1;31;40m] \033[1;36;40mGithub \n \033[1;31;40m[\033[1;33;40m5\033[1;31;40m] \033[1;36;40mExit \n'
+print(s)
+
+
+mo = input('\033[1;33;40mEnter \033[1;31;40m>>>\033[1;33;40m ')
+  
+  
+if mo == '1':
+  os.system('clear')
+  print ('Allow the Button For Access the Storage in Termux')
+  os.system('termux-setup-storage')
+  os.system ("apt upgrade -y")
+  os.system('pkg install python2')
+  os.system('pip install --upgrade pip')
+  os.system('pip install requests')
+  os.system('pip install mechanize')
+  os.system('pip2 install requests')
+  os.system('pip2 install mechanize')
+  os.system('pip2 install bs4')
+  os.system('pip install lolcat')
+  os.system('pip2 install lolcat')
+  os.system('pkg install wget')
+  os.system('pkg install php -y')
+  os.system('pkg install nano -y')
+  os.system('pkg installcurl -y')
+  os.system('pkg install tor -y')
+  os.system('pkg install openssh')
+  os.system('pkg install bash')
+  os.system ("apt install python-dev -y")
+  os.system('apt install ruby -y')
+  os.system('apt install perl -y')
+  os.system('apt install nmap -y')
+  os.system('apt install clang -y')
+  os.system('apt install jq -y')
+  os.system('apt install macchanger -y')
+  os.system('apt install tar -tar')
+  os.system('apt install zip -y')
+  os.system('apt install unzip -y')
+  os.system('apt install bmon -y')
+  os.system ("apt install proot -y")
+  os.system ("apt install java -y")
+  os.system ("apt install figlet -y")
+  os.system('xdg-open https://www.facebook.com/profile.php?id=100081753736308&mibextid=ZbWKwL')
+  os.system('clear')
+  print(logo2)
+  print('All Basic Pkg Install successful\n')
+  input('\nPress the enter key to exit ')
+  
+elif mo == '2':
+  os.system('clear')
+  print(logo2)
+  slowprint('''
+[01]  termux-setup-storage
+[02]  apt upgrade -y
+[03]  pkg install python2
+[04]  pip install --upgrade pip
+[05]  pip install requests
+[06]  pip install mechanize
+[07]  pip2 install requests
+[08]  pip2 install mechanize
+[09]  pip2 install bs4
+[10]  pip install lolcat
+[11]  pip2 install lolcat
+[12]  pkg install wget
+[13]  pkg install php -y
+[14]  pkg install nano -y
+[15]  pkg installcurl -y
+[16]  pkg install tor -y
+[17]  pkg install openssh
+[18]  pkg install bash
+[19]  apt install python-dev -y
+[20]  apt install ruby -y
+[21]  apt install perl -y
+[22]  apt install nmap -y
+[23]  apt install clang -y
+[24]  apt install jq -y
+[25]  apt install macchanger -y
+[26]  apt install tar -tar
+[27]  apt install zip -y
+[28]  apt install unzip -y
+[28]  apt install bmon -y
+[29]  apt install proot -y
+[30]  apt install java -y
+[31]  apt install figlet -y
+  ''')
+  m = input('Enter To Continue ')
+  if m == '':
+    os.system('python MAK-Tbasic.py')
+  
+elif mo == '3':
+  os.system('xdg-open https://www.facebook.com/profile.php?id=100081753736308&mibextid=ZbWKwL')
+elif mo == '4':
+  os.system('xdg-open https://github.com/Emran-cyber99487')
+elif mo == '5':
+  os.system('exit')
+  
